fa0b66fe56831d3f333d581c780d7cbabd76362f
Joshua <Naidoo>
2019-10-04 16:47:37 +0200

    Finished tut 6, code for reuse is in Department.cshtml line 42. Might need to edit _AdminLayout since it is shared.

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using BookKeeperNewJosh.DAL;
+using BookKeeperNewJosh.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookKeeperNewJosh.Controllers
+{
+    public class AdminController : Controller
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+        public ActionResult Department()
+        {
+            List<Department> alldepartments = _unitOfWork.GetRepositoryInstance<Department>().GetAllRecordsIQueryable().ToList();
+            return View(alldepartments);
+        }
+    }
+}
