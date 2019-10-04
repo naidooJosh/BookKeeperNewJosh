157f071b17dd96c3feb3b4fb91a1aa97758516e4
Joshua <Naidoo>
2019-10-04 05:47:15 +0200

    Finished tut4, possible to need more classes

---
+++ Models/BookListingDetails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace BookKeeperNewJosh.Models
+{
+    public class BookListingDetails
+    {
+        public int listingID { get; set; }
+        [Required]
+        public decimal listedPrice { get; set; }
+        public decimal soldPrice { get; set; }
+        [Required]
+        public int condition { get; set; }
+        [Required(ErrorMessage ="Please enter the book title")]
+        public string title { get; set; }
+        [Required(ErrorMessage = "Please enter the author's name")]
+        public string author { get; set; }
+        [Required(ErrorMessage = "Please enter the book title")]
+        public string studentNumber { get; set; }
+        
+        [Required]
+
+        public string moduleCode { get; set; }
+        [Required]
+        public SelectList department { get; set; }
+        public byte[] photo { get; set; }
+        public string description { get; set; }
+        public bool isSold { get; set; }
+        public bool isQuickSell { get; set; }
+        public Nullable<float> perDrop { get; set; }
+        public Nullable<int> duration { get; set; }
+    }
+
+ 
+}
---
+++ Models/Shopping.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookKeeperNewJosh.Models
+{
+    public class ShippingDetails
+    {
+        public string shippingDetailID { get; set; }
+        [Required]
+        public string studentNo { get; set; }
+        [Required]
+        public string address { get; set; }
+        [Required]
+        public string city { get; set; }
+        [Required]
+        public string state { get; set; }
+        [Required]
+        public string country { get; set; }
+        [Required]
+        public string zipCode { get; set; }
+        [Required]
+        public int orderID { get; set; }
+       
+        public decimal amountPaid { get; set; }
+        [Required]
+        public string paymentType { get; set; }
+
+      
+    }
+}
