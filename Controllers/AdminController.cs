16771c2f8dffc21191a7fb8aa814a9880b28838f
Mcbey, Jesse, (Mr) (s216394139) <s216394139@Mandela.ac.za>
2019-10-05 14:10:54 +0200

    Beginning of Admin and product layout setup

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using BookKeeperNewJosh.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookKeeperNewJosh.Controllers
+{
+    public class AdminController : Controller
+    {
+
+
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+
+        //* NB PLZ right click the lightbulb of the underlined List, and click "using BookKeeperDAL;"
+        public ActionResult Module()
+        {
+            List<Tbl_Module> allModules = _unitOfWork.GetRepositoryInstance<Tbl_Module>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+        }
+    }
+}
